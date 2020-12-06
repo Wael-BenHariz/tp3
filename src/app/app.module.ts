d39552c04b92b73d144b1f57c9f887a854035050
Wael-BenHariz <waelbhz@gmail.com>
2020-12-06 16:12:17 +0100

    projetInitial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AccueilComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
